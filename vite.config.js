a69abc6464dcc0b9779bb3795b60778f43147caa
mr.rivers <erivers@salpointe.org>
2025-11-27 14:54:49 -0700

    Add PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/salpointe-cte-hub/", // This matches your future GitHub repo name
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Salpointe CTE Hub',
+        short_name: 'CTE Hub',
+        description: 'Request equipment and services from Salpointe CTE.',
+        theme_color: '#881E27', // Salpointe Maroon
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'pwa-icon.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-icon.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
+  base: "/salpointe-cte-hub/",
 })
